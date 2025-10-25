570522d18a9ee54dcaf4daaa22fc4260815d0df3
unknown <bastien.harmand@mines-ales.org>
2025-10-25 15:20:42 +0200

    add hours slider, update figure

--- render.py
+++ render.py
@@ -1,23 +1,31 @@
-import streamlit as st
 import base64
+import streamlit as st
 from datetime import timedelta, time, date
-import textwrap
 
 
 def render_svg(svg):
     """Renders the given svg string."""
     b64 = base64.b64encode(svg.encode("utf-8")).decode("utf-8")
     html = r'<center><img src="data:image/svg+xml;base64,%s"/></center>' % b64
-    st.image(svg)#, unsafe_allow_html=True)
+    st.image(svg)  # , unsafe_allow_html=True)
 
 
-def rotate_svg(svg, angle):
+def rotate_svg(svg, angle, angle_vis):
     offset = int(887.04 / 2)
 
     return svg.replace(
         '<g id="rete">',
         '<g id="rete" transform="rotate('
-        + str(angle + int(360 * 11 / 365))
+        + str(angle + int(360 * 11 / 365) + angle_vis / 360)
+        + ", "
+        + str(offset)
+        + ", "
+        + str(offset)
+        + ')">',
+    ).replace(
+        '<g id="reticule">',
+        '<g id="reticule" transform="rotate('
+        + str(angle_vis)
         + ", "
         + str(offset)
         + ", "
@@ -29,60 +37,88 @@ def rotate_svg(svg, angle):
 if __name__ == "__main__":
     st.set_page_config(layout="wide")
 
-    if "date_numeric" not in st.session_state:
-        st.session_state["date_numeric"] = date.today()
-    if "date_slider" not in st.session_state:
-        st.session_state["date_slider"] = (
-            st.session_state.date_numeric
-            - date(st.session_state.date_numeric.year, 1, 1)
-        ).days
+    with st.spinner("..."):
 
-    def update_date_slider():
-        st.session_state.date_slider = (
-            st.session_state.date_numeric
-            - date(st.session_state.date_numeric.year, 1, 1)
-        ).days
+        if "date_numeric" not in st.session_state:
+            st.session_state["date_numeric"] = date.today()
+        if "date_slider" not in st.session_state:
+            st.session_state["date_slider"] = (
+                st.session_state.date_numeric
+                - date(st.session_state.date_numeric.year, 1, 1)
+            ).days
+        if "time_numeric" not in st.session_state:
+            st.session_state["time_numeric"] = time()
+        if "time_slider" not in st.session_state:
+            st.session_state["time_slider"] = time()
 
-    def update_date_numeric():
-        st.session_state.date_numeric = date(
-            st.session_state.date_numeric.year, 1, 1
-        ) + timedelta(days=st.session_state.date_slider)
+        def update_date_slider():
+            st.session_state.date_slider = (
+                st.session_state.date_numeric
+                - date(st.session_state.date_numeric.year, 1, 1)
+            ).days
 
-    f = open("figures/combined.svg")
-    svg = f.read()
-    f.close()
+        def update_date_numeric():
+            st.session_state.date_numeric = date(
+                st.session_state.date_numeric.year, 1, 1
+            ) + timedelta(days=st.session_state.date_slider)
 
-    with st.sidebar:
-        with st.container(border=True):
-            date_select = st.date_input(
-                "Date", date.today(), key="date_numeric", on_change=update_date_slider
+        def update_time_slider():
+            st.session_state.time_slider = st.session_state.time_numeric
+
+        def update_time_numeric():
+            st.session_state.time_numeric = st.session_state.time_slider
+
+        f = open("figures/combined.svg")
+        svg = f.read()
+        f.close()
+
+        with st.sidebar:
+            with st.container(border=True):
+                date_numeric = st.date_input(
+                    "Date", key="date_numeric", on_change=update_date_slider
+                )
+                date_slider = st.slider(
+                    "Day of Year",
+                    # value=(date_numeric - date(date_numeric.year, 1, 1)).days,
+                    min_value=0,
+                    max_value=365,
+                    key="date_slider",
+                    on_change=update_date_numeric,
+                )
+                angle_rete = int(
+                    360 * (date_numeric - date(date_numeric.year, 1, 1)).days / (365)
+                )
+                st.text(f"Mere rotation : {angle_rete}°")
+
+            with st.container(border=True):
+                time_numeric = st.time_input(
+                    "Time", key="time_numeric", on_change=update_time_slider
+                )
+                time_slider = st.slider(
+                    "Time",
+                    min_value=time(0, 0),
+                    max_value=time(23, 59),
+                    key="time_slider",
+                    on_change=update_time_numeric,
+                )
+                angle_vis = int(
+                    360 * (time_numeric.hour + time_numeric.minute / 60) / 24
+                )
+                st.text(f"Day rotation : {angle_vis}°")
+
+            st.download_button(
+                label="⬇ Download SVG",
+                data=rotate_svg(svg, angle_rete, angle_vis),
+                file_name="astrolabe.svg",
             )
-            time_select = st.slider(
-                "Day of Year",
-                value=(date_select - date(date_select.year, 1, 1)).days,
-                min_value=0,
-                max_value=365,
-                key="date_slider",
-                on_change=update_date_numeric,
-            )
-            angle = int(360 * (date_select - date(date_select.year, 1, 1)).days / (365))
-            st.text(f"Mere rotation : {angle}°")
 
-        st.download_button(
-            label="⬇ Download SVG",
-            data=rotate_svg(svg, angle),
-            file_name="astrolabe.svg",
-        )
-
-    col1, col2, col3 = st.columns([1, 10, 1])
-
-    with col1:
-        st.write(" ")
-
-    with col2:
-        with st.container(border=True):
-            st.write(date_select)
-            render_svg(rotate_svg(svg, angle))
-
-    with col3:
-        st.write(" ")
+        col1, col2, col3 = st.columns([1, 10, 1])
+        with col1:
+            st.write(" ")
+        with col2:
+            with st.container(border=True):
+                st.write(date_numeric)
+                st.write(time_slider)
+                render_svg(rotate_svg(svg, angle_rete, angle_vis))
+        with col3:
+            st.write(" ")
